fa8bf8858cc08ebc2f130fb460dd725a0406543a
amitabra123 <amit271005@gmail.com>
2024-05-15 21:25:15 +0300

    added colors to outputs in the bonus part

--- BonusPart2048/ConsoleGame.cs
+++ BonusPart2048/ConsoleGame.cs
@@ -23,15 +23,19 @@ namespace BonusPart2048
             if(game2048.TheGameStatus == GameStatus.Lose)
             {
                 ShowGameBoard();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Sorry you lost!");
                 Console.WriteLine("Bye Bye");
+                Console.ForegroundColor = ConsoleColor.White;
                 return;
             }    
             else if(!userChoseToExit)
             {
                 ShowGameBoard();
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Wow!! you won (2048!)");
                 Console.WriteLine("Bye Bye");
+                Console.ForegroundColor = ConsoleColor.White;
                 return;
             }
         }
@@ -50,20 +54,25 @@ namespace BonusPart2048
 
         public void UserChooseDirection()
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("please choose a direction!- to exit press enter");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
             ConsoleKey userChooseDirection = Console.ReadKey().Key;
             while (userChooseDirection != ConsoleKey.UpArrow && userChooseDirection != ConsoleKey.DownArrow &&
                 userChooseDirection != ConsoleKey.RightArrow && userChooseDirection != ConsoleKey.LeftArrow &&
                 userChooseDirection != ConsoleKey.Enter)
             {
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("\n please choose a valid direction (UpArrow, DownArrow, LeftArrow or RightArrow)!!");
                 userChooseDirection = Console.ReadKey().Key;
-
+                Console.ForegroundColor = ConsoleColor.White;
             }
             if (userChooseDirection == ConsoleKey.Enter)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("YOU CHOSE TO EXIT!");
                 Console.WriteLine("BYE BYE!");
+                Console.ForegroundColor = ConsoleColor.White;
                 userChoseToExit = true;
                 return;
             }
@@ -75,25 +84,33 @@ namespace BonusPart2048
         {
             if (userChooseDirection == ConsoleKey.UpArrow)
             {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("UP");
+                Console.ForegroundColor = ConsoleColor.Gray;
                 game2048.Move(Direction.Up);
                 Console.WriteLine("points: " + game2048.Points);
             }
             else if (userChooseDirection == ConsoleKey.DownArrow)
             {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("DOWN");
+                Console.ForegroundColor = ConsoleColor.Gray;
                 game2048.Move(Direction.Down);
                 Console.WriteLine("points: " + game2048.Points);
             }
             else if (userChooseDirection == ConsoleKey.RightArrow)
             {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("RIGHT");
                 game2048.Move(Direction.Right);
+                Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine("points: " + game2048.Points);
             }
             else if (userChooseDirection == ConsoleKey.LeftArrow)
             {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("LEFT");
+                Console.ForegroundColor = ConsoleColor.Gray;
                 game2048.Move(Direction.Left);
                 Console.WriteLine("points: " + game2048.Points);
             }
